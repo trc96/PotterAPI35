3f18823016f1c20a758bbc6dc3cdb6bb25686b3f
Todd Crandall <toddcrandall1996@gmail.com>
2020-08-05 13:57:34 -0600

    This is my first commit.

---
+++ PotterAPI35/PotterAPI35/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PotterAPI35
+//
+//  Created by Todd Crandall on 8/5/20.
+//  Copyright © 2020 Todd Crandall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
